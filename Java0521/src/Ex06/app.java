d0d27b1e60dee91325afb47422aaefaa06f403a0
xero_xer0 <narukami2602@gmail.com>
2024-05-21 15:50:31 +0900

    Directory

--- Java0521/src/Ex06/app.java
+++ Java0521/src/Ex06/app.java
@@ -1,9 +1,22 @@
 package Ex06;
+
 import java.io.*;
 import java.util.*;
+
 public class app {
     public static void main(String[] args) {
-        File path = new File(".\\");
-        
+        File path = new File("./");
+        System.out.println(path.getAbsolutePath());
+
+        File[] subFiles = path.listFiles();
+        for (File file : subFiles) {
+            long t = file.lastModified();
+            if (file.isDirectory()) {
+                System.out.println("[" + file.getName() + "]");
+            } else {
+                System.out.printf("%tb %td %ta %tT \n", t,t,t,t);
+                System.out.println(file.getName() + "\t" + file.length()+" bytes");
+            }
+        }
     }
 }
