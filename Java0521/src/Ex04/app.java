62384fde85b58ecf9c7be8bb3167a5ce6fe246bf
xero_xer0 <narukami2602@gmail.com>
2024-05-21 14:46:37 +0900

    Write binary file

--- Java0521/src/Ex04/app.java
+++ Java0521/src/Ex04/app.java
@@ -2,6 +2,16 @@ package Ex04;
 import java.io.*;
 public class app {
     public static void main(String[] args) {
-        
+        byte b[] = {7, 51, 3, 4, -1, 24};
+        try{
+            FileOutputStream fos = new FileOutputStream("test.out");
+            for(int i = 0; i < b .length; i++){
+                fos.write(b[i]);
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("save OK!");
     }
 }
