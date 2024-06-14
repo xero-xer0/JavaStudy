eb7dc232c7832fbc52319d797adc47897e4780e8
xero_xer0 <narukami2602@gmail.com>
2024-06-14 16:57:46 +0900

    도구 선택 시 이탤릭체로 표시되도록 수정. 도구 순서 수정. 지우개 크기 변경/선 두께변경 기능 추가

--- JavaFinalProject/src/Painter.java
+++ JavaFinalProject/src/Painter.java
@@ -8,14 +8,18 @@ import java.io.IOException;
 
 public class Painter extends JFrame {
     private DrawArea drawArea;
-    private JButton clearButton, colorButton, eraserButton, lineButton, rectButton, ovalButton, saveButton, loadButton;
+    private JButton clearButton, colorButton, eraserButton, lineButton, rectButton, ovalButton, saveButton, loadButton,
+            freeformButton;
+    private JSpinner lineThicknessSpinner, eraserSizeSpinner;
     private Color currentColor = Color.BLACK;
-    private String currentTool = "Brush";
-    private int startX, startY, endX, endY;
+    private String currentTool = "Freeform";
+    private int startX, startY;
+    private int lineThickness = 1;
+    private int eraserSize = 10;
 
     public Painter() {
         setTitle("Painter");
-        setSize(800, 600);
+        setSize(1200, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -25,46 +29,69 @@ public class Painter extends JFrame {
         JPanel controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
 
-        clearButton = new JButton("Clear");
-        clearButton.addActionListener(e -> drawArea.clear());
-        controlPanel.add(clearButton);
-
-        colorButton = new JButton("Color");
+        colorButton = new JButton("색상");
         colorButton.addActionListener(e -> chooseColor());
         controlPanel.add(colorButton);
 
-        eraserButton = new JButton("Eraser");
-        eraserButton.addActionListener(e -> currentTool = "Eraser");
-        controlPanel.add(eraserButton);
+        freeformButton = new JButton("펜");
+        freeformButton.addActionListener(e -> setCurrentTool("Freeform"));
+        controlPanel.add(freeformButton);
 
-        lineButton = new JButton("Line");
-        lineButton.addActionListener(e -> currentTool = "Line");
+        lineButton = new JButton("직선");
+        lineButton.addActionListener(e -> setCurrentTool("Line"));
         controlPanel.add(lineButton);
 
-        rectButton = new JButton("Rectangle");
-        rectButton.addActionListener(e -> currentTool = "Rectangle");
+        rectButton = new JButton("직사각형");
+        rectButton.addActionListener(e -> setCurrentTool("Rectangle"));
         controlPanel.add(rectButton);
 
-        ovalButton = new JButton("Oval");
-        ovalButton.addActionListener(e -> currentTool = "Oval");
+        ovalButton = new JButton("타원");
+        ovalButton.addActionListener(e -> setCurrentTool("Oval"));
         controlPanel.add(ovalButton);
 
-        saveButton = new JButton("Save");
+        eraserButton = new JButton("지우개");
+        eraserButton.addActionListener(e -> setCurrentTool("Eraser"));
+        controlPanel.add(eraserButton);
+
+        controlPanel.add(new JLabel("선 두께 "));
+        lineThicknessSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 20, 1));
+        lineThicknessSpinner.addChangeListener(e -> lineThickness = (int) lineThicknessSpinner.getValue());
+        controlPanel.add(lineThicknessSpinner);
+
+        controlPanel.add(new JLabel("지우개 크기"));
+        eraserSizeSpinner = new JSpinner(new SpinnerNumberModel(10, 1, 50, 1));
+        eraserSizeSpinner.addChangeListener(e -> eraserSize = (int) eraserSizeSpinner.getValue());
+        controlPanel.add(eraserSizeSpinner);
+
+        saveButton = new JButton("저장");
         saveButton.addActionListener(e -> saveImage());
         controlPanel.add(saveButton);
 
-        loadButton = new JButton("Load");
+        loadButton = new JButton("열기");
         loadButton.addActionListener(e -> loadImage());
         controlPanel.add(loadButton);
 
+        clearButton = new JButton("전체 삭제");
+        clearButton.addActionListener(e -> drawArea.clear());
+        controlPanel.add(clearButton);
+
         add(controlPanel, BorderLayout.NORTH);
+
+        addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                drawArea.resizeImage(getWidth(), getHeight());
+            }
+        });
+
+        // Set default selected tool
+        setCurrentTool("Freeform");
     }
 
     private void chooseColor() {
         Color newColor = JColorChooser.showDialog(this, "Choose Color", currentColor);
         if (newColor != null) {
             currentColor = newColor;
-            currentTool = "Brush";
+            setCurrentTool("Freeform");
         }
     }
 
@@ -97,6 +124,39 @@ public class Painter extends JFrame {
         }
     }
 
+    private void setCurrentTool(String tool) {
+        currentTool = tool;
+        updateButtonStyles();
+    }
+
+    private void updateButtonStyles() {
+        JButton[] buttons = { freeformButton, lineButton, rectButton, ovalButton, eraserButton };
+        for (JButton button : buttons) {
+            if (button.getText().equals(getToolName(currentTool))) {
+                button.setFont(button.getFont().deriveFont(Font.ITALIC));
+            } else {
+                button.setFont(button.getFont().deriveFont(Font.PLAIN));
+            }
+        }
+    }
+
+    private String getToolName(String tool) {
+        switch (tool) {
+            case "Freeform":
+                return "펜";
+            case "Line":
+                return "직선";
+            case "Rectangle":
+                return "직사각형";
+            case "Oval":
+                return "타원";
+            case "Eraser":
+                return "지우개";
+            default:
+                return "";
+        }
+    }
+
     private class DrawArea extends JPanel {
         private BufferedImage image;
         private Graphics2D g2;
@@ -107,9 +167,7 @@ public class Painter extends JFrame {
             image = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
             g2 = image.createGraphics();
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setPaint(Color.white);
-            g2.fillRect(0, 0, 800, 600);
-            g2.setPaint(Color.black);
+            clear();
 
             addMouseListener(new MouseAdapter() {
                 public void mousePressed(MouseEvent e) {
@@ -117,9 +175,11 @@ public class Painter extends JFrame {
                     prevY = e.getY();
                     startX = e.getX();
                     startY = e.getY();
+                    drawPoint(startX, startY);
                 }
 
                 public void mouseReleased(MouseEvent e) {
+                    g2.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
                     if (currentTool.equals("Line")) {
                         g2.setPaint(currentColor);
                         g2.drawLine(startX, startY, e.getX(), e.getY());
@@ -141,14 +201,14 @@ public class Painter extends JFrame {
                     x = e.getX();
                     y = e.getY();
 
-                    if (currentTool.equals("Brush")) {
+                    g2.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                    if (currentTool.equals("Freeform")) {
                         g2.setPaint(currentColor);
                         g2.drawLine(prevX, prevY, x, y);
                     } else if (currentTool.equals("Eraser")) {
                         g2.setPaint(Color.white);
-                        g2.setStroke(new BasicStroke(10));
+                        g2.setStroke(new BasicStroke(eraserSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
                         g2.drawLine(prevX, prevY, x, y);
-                        g2.setStroke(new BasicStroke(1));
                     }
 
                     prevX = x;
@@ -169,7 +229,6 @@ public class Painter extends JFrame {
             g2.fillRect(0, 0, getWidth(), getHeight());
             g2.setPaint(Color.black);
             repaint();
-            currentTool = "Brush";
         }
 
         public BufferedImage getImage() {
@@ -180,6 +239,29 @@ public class Painter extends JFrame {
             this.image = image;
             g2 = this.image.createGraphics();
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            repaint();
+        }
+
+        public void resizeImage(int newWidth, int newHeight) {
+            if (newWidth > 0 && newHeight > 0) {
+                BufferedImage newImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g2d = newImage.createGraphics();
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setPaint(Color.white);
+                g2d.fillRect(0, 0, newWidth, newHeight);
+                g2d.drawImage(image, 0, 0, null);
+                g2d.dispose();
+                image = newImage;
+                g2 = image.createGraphics();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                repaint();
+            }
+        }
+
+        private void drawPoint(int x, int y) {
+            g2.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+            g2.setPaint(currentColor);
+            g2.drawLine(x, y, x, y);
             repaint();
         }
     }
